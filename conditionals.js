287ad07ca3c00b0e5e6c3c369ccc7c6b9c1aaa19
Bharanitharan-T <bharani8778@gmail.com>
2025-06-19 11:22:09 +0530

    complete conditionals

---
+++ conditionals.js
@@ -0,0 +1,10 @@
+let marks =85;
+if(marks<= 90){
+    console.log("A grade");
+}
+else if(marks>=75){
+    console.log("B grade");
+}
+else{
+    console.log("C grade");
+}
---
+++ data-type.js
@@ -0,0 +1,5 @@
+let name="Jhon Doe"
+let age=25;
+let price=150.25;
+let isStudent=false;
+console.log(price)
